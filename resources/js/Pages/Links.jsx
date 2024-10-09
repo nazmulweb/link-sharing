94fa5167d116dd7ea797d3c913a7b49f891d2ecc
mdnazmul.hasan <mdnazmul.hasan@bracits.com>
2024-10-10 03:24:52 +0600

    left side mobile display design is done

--- resources/js/Pages/Links.jsx
+++ resources/js/Pages/Links.jsx
@@ -1,18 +1,20 @@
 import Card from '@/Components/Card'
 import Col from '@/Components/Col'
 import Grid from '@/Components/Grid'
+import MobileDisplay from '@/Components/MobileDisplay'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
-import { Head } from '@inertiajs/react'
+import { Head, usePage } from '@inertiajs/react'
 import React from 'react'
 
 const Links = () => {
+  const {name, email} = usePage().props.auth.user;
   return (
     <AuthenticatedLayout>
         <Head title="Links" />
         <Grid>
             <Col col={2} className='hidden lg:block'>
                 <Card className="p-5">
-                    link page left side
+                    <MobileDisplay name={name} email={email} />
                 </Card>
             </Col>
             <Col col={3}>
