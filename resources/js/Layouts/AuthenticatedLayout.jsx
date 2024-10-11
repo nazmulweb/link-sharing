a7e4c459e0093b02256f9f9b563ef8565020b81b
mdnazmul.hasan <mdnazmul.hasan@bracits.com>
2024-10-11 06:53:00 +0600

    guest layout and athenticaed layout page update

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -10,56 +10,68 @@ export default function AuthenticatedLayout({ header, children }) {
             <nav className="border-b border-gray-100 bg-white sticky top-6 rounded-lg z-20">
                 <div className="mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex h-16 justify-between">
-                        <div className="flex items-center justify-between w-full">
-                            <div className="flex shrink-0 items-center">
-                                <Link href={route('links.index')}>
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                                </Link>
-                            </div>
+                        {
+                            route().current('links.preview') ? 
+                            <div className="flex items-center justify-between w-full">
+                                <NavLink href={route('links.index')}>
+                                    <SecondaryButton border>Back to Editor</SecondaryButton>
+                                </NavLink>
+                                <SecondaryButton solid>Share Link</SecondaryButton>
+                            </div> 
+                            
+                             : 
+                        
+                            <div className="flex items-center justify-between w-full">
+                                <div className="flex shrink-0 items-center">
+                                    <Link href={route('links.index')}>
+                                        <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    </Link>
+                                </div>
 
-                            <div className="space-x-6 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink
-                                    href={route('links.index')}
-                                >
-                                    <SecondaryButton active={route().current('links.index')}>
-                                        <i class="bi bi-link-45deg"></i>
-                                        <span className=' hidden md:block'>
-                                            Links
-                                        </span>
-                                    </SecondaryButton>
-                                </NavLink>
-                                <NavLink
-                                    href={route('profile.edit')}
-                                >
-                                    <SecondaryButton active={route().current('profile.edit')}>
-                                        <i class="bi bi-person"></i>
-                                        <span className='hidden md:block'>Profile Details</span>
-                                    </SecondaryButton>
-                                </NavLink>
-                            </div>
-                            <div className="space-x-6 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink
-                                    href={route('links.preview')}
-                                    
-                                >
-                                    <SecondaryButton border active={route().current('links.preview')}>
-                                        <i class="bi bi-eye block md:hidden"></i>
-                                        <span className='hidden md:block'>Preview</span>
-                                    </SecondaryButton>
-                                </NavLink>
+                                <div className="space-x-6 sm:-my-px sm:ms-10 sm:flex">
+                                    <NavLink
+                                        href={route('links.index')}
+                                    >
+                                        <SecondaryButton active={route().current('links.index')}>
+                                            <i class="bi bi-link-45deg"></i>
+                                            <span className=' hidden md:block'>
+                                                Links
+                                            </span>
+                                        </SecondaryButton>
+                                    </NavLink>
+                                    <NavLink
+                                        href={route('profile.edit')}
+                                    >
+                                        <SecondaryButton active={route().current('profile.edit')}>
+                                            <i class="bi bi-person"></i>
+                                            <span className='hidden md:block'>Profile Details</span>
+                                        </SecondaryButton>
+                                    </NavLink>
+                                </div>
+                                <div className="space-x-6 sm:-my-px sm:ms-10 sm:flex">
+                                    <NavLink
+                                        href={route('links.preview')}
+                                        
+                                    >
+                                        <SecondaryButton border active={route().current('links.preview')}>
+                                            <i class="bi bi-eye block md:hidden"></i>
+                                            <span className='hidden md:block'>Preview</span>
+                                        </SecondaryButton>
+                                    </NavLink>
 
-                                <NavLink
-                                    method="post"
-                                    href={route('logout')}
-                                    as="button"
-                                >
-                                <SecondaryButton border className=' border-red-300 text-red-400'>
-                                    <i class="bi bi-box-arrow-in-right block md:hidden"></i>
-                                    <span className='hidden md:block'>Log Out</span>
-                                </SecondaryButton>
-                                </NavLink>
+                                    <NavLink
+                                        method="post"
+                                        href={route('logout')}
+                                        as="button"
+                                    >
+                                    <SecondaryButton border className=' border-red-300 text-red-400'>
+                                        <i class="bi bi-box-arrow-in-right block md:hidden"></i>
+                                        <span className='hidden md:block'>Log Out</span>
+                                    </SecondaryButton>
+                                    </NavLink>
+                                </div>
                             </div>
-                        </div>
+                        }
                     </div>
                 </div>
             </nav>
